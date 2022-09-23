7aef5fcf99d61ceca0ccf6f7d571501724187784
--global <h4ystack@gmail.com>
2022-09-24 00:23:08 +0900

    add simple initialize script

---
+++ scripts/init.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# USAGE: make symlink to this file in some directory that is contained PATH
+echo '\e[36m###\e[m this is simple copy program \e[36m###\e[m'
+
+copy_to_current () {
+    if [ ! -e "$PWD/$1" ]; then
+        cp -r $REPO_ROOT/$1 $PWD/$1
+        echo "\e[32m>>>\e[m made new directory $1" 1>&2
+    else
+        echo "\e[31m!!!\e[m $1 already exists, so not copied" 1>&2
+    fi
+}
+
+REPO_ROOT=$(dirname $(dirname $(realpath $0)))
+copy_to_current .vscode
+copy_to_current .devcontainer
