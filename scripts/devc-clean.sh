de2a86bb21c4e82367f96ea5744f315cc33c4beb
hayas1 <h4ystack@gmail.com>
2022-09-28 13:56:47 +0000

    clean to core feature

--- scripts/devc-clean.sh
+++ scripts/devc-clean.sh
@@ -1,27 +1,9 @@
-#!/bin/sh -e
-echo '\e[36m###\e[m this is simple remove program \e[36m###\e[m'
+#!/bin/bash -e
 
-remove_current() {
-    if [ ! -e "$PWD/$1" ]; then
-        echo "\e[32m!!!\e[m no file $1" 1>&2
-    else
-        echo -n "\e[31m>>>\e[m really remove $1? [y/N]"
-        read ans
-        case $ans in
-        [Yy]*)
-            mv "$PWD"/"$1" "$tmp_dir"
-            echo "\e[31m...\e[m file $1 moved to $tmp_dir"
-            echo "\e[35m^^^ want to recover, try \$ mv $tmp_dir/.* $PWD\e[m"
-            ;;
-        *)
-            echo "\e[32m!!!\e[m do not remove $1"
-            ;;
-        esac
-    fi
+usage() {
+    # TODO remove all tmp file
+    echo "usage of clean"
 }
 
-timestamp=$(date +%Y%m%d%H%M%S)
-tmp_dir=$(mktemp -dt "dotfiles.remove.$timestamp.XXXXXX")
-
-remove_current .vscode
-remove_current .devcontainer
+SCRIPT_ROOT="$(dirname "$(realpath "$0")")"
+"$SCRIPT_ROOT"/core/devc-clean.sh "${@:2}" || usage
