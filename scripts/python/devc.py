2466ac8285b4d7a938c03dc37d3130ad4519cece
hayas1 <h4ystack@gmail.com>
2022-09-29 23:21:24 +0000

    subcommands in future

--- scripts/python/devc.py
+++ scripts/python/devc.py
@@ -6,6 +6,22 @@ def init(args):
     print("init args:", args)
 
 
+def migrate(args):
+    print("migrate is developing...", args)
+
+
+def clean(args):
+    print("clean args:", args)
+
+
+def prune(args):
+    print("prune args:", args)
+
+
+def update(args):
+    print("update args:", args)
+
+
 # TODO use dataclass or Enum
 FEATURES = [
     "python",
@@ -31,6 +47,22 @@ if __name__ == "__main__":
     init_parser.add_argument("--gitignore", action="store_true", help="Make gitignore in copied directory")
     init_parser.add_argument("--features", choices=FEATURES, help="Initialize workspace with features")
     init_parser.set_defaults(handler=init)
+
+    migrate_parser = subparsers.add_parser("migrate", help="Migrate workspace (developing)")
+    migrate_parser.add_argument("-f", "--force", action="store_true", help="Force migration, overwrite workspace")
+    migrate_parser.set_defaults(handler=migrate)
+
+    clean_parser = subparsers.add_parser("clean", help="Clean workspace")
+    clean_parser.add_argument("-y", "--yes", action="store_true", help="Clean workspace without confirmation")
+    clean_parser.set_defaults(handler=clean)
+
+    prune_parser = subparsers.add_parser("prune", help="Prune temporary directories")
+    prune_parser.add_argument("-y", "--yes", action="store_true", help="Without confirmation")
+    prune_parser.set_defaults(handler=prune)
+
+    update_parser = subparsers.add_parser("update", help="Update local repository")
+    update_parser.add_argument("-f", "--force", action="store_true", help="Uninstall once, and clone again")
+    update_parser.set_defaults(handler=update)
 
     help_parser = subparsers.add_parser("help", help="Help")
     help_parser.add_argument("command", help="Command help")
