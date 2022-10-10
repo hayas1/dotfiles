623aacb925ca6eb3b18205cb25a241fd8167a5ee
hayas1 <h4ystack@gmail.com>
2022-10-10 02:16:05 +0000

    modify path

--- tests/container/installed-zsh.sh
+++ tests/container/installed-zsh.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
-zsh -c exit && cat /home/vscode/.oh-my-zsh/custom/themes/powerlevel10k/internal/p10k.zsh >/dev/null &&
-    diff /workspaces/dotfiles/.devcontainer/resources/.p10k.zsh ~/.p10k.zsh 1>&2 &&
-    diff /workspaces/dotfiles/.devcontainer/resources/.bash_aliases ~/.bash_aliases 1>&2
+zsh -c exit && test -e "${HOME}/.oh-my-zsh/custom/themes/powerlevel10k/internal/p10k.zsh" &&
+    diff "${WORKSPACE}/.devcontainer/resources/.p10k.zsh" "${HOME}/.p10k.zsh" 1>&2 &&
+    diff "${WORKSPACE}/.devcontainer/resources/.bash_aliases" "${HOME}/.bash_aliases" 1>&2
