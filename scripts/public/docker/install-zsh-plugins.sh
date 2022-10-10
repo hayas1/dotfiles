8bcc3072d952466c35b5494c486ee2d64a5a8977
hayas1 <h4ystack@gmail.com>
2022-10-10 16:33:21 +0000

    use no ~

--- scripts/public/docker/install-zsh-plugins.sh
+++ scripts/public/docker/install-zsh-plugins.sh
@@ -9,30 +9,30 @@ if ! (type zsh >/dev/null 2>&1); then
     apt-get update -y && apt-get install -y zsh && apt-get clean && rm -rf /var/lib/apt/lists
 fi
 
-if [ -e "${HOME}/.oh-my-zsh" ]; then
+if [ -e "$HOME/.oh-my-zsh" ]; then
     ### if exist oh-my-zsh, use it
-    if [ ! -e ~/.zshrc ]; then
+    if [ ! -e "$HOME/.zshrc" ]; then
         # if no .zshrc, get from oh-my-zsh template
-        curl -fsL 'https://github.com/ohmyzsh/ohmyzsh/raw/master/templates/zshrc.zsh-template' -o "${HOME}/.zshrc"
+        curl -fsL 'https://github.com/ohmyzsh/ohmyzsh/raw/master/templates/zshrc.zsh-template' -o "$HOME/.zshrc"
     fi
-    echo '### zsh plugins' >>"${HOME}/.zshrc"
+    echo '### zsh plugins' >>"$HOME/.zshrc"
 
     # powerlevel10k
     git clone --depth=1 'https://github.com/romkatv/powerlevel10k.git' "${ZSH_CUSTOM:-${ZSH:-$HOME/.oh-my-zsh}/custom}/themes/powerlevel10k"
-    sed -ri 's/^\s*ZSH_THEME="(.*)"/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' "${HOME}/.zshrc"
+    sed -ri 's/^\s*ZSH_THEME="(.*)"/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' "$HOME/.zshrc"
 
     # zsh-completions https://github.com/zsh-users/zsh-completions
-    git clone https://github.com/zsh-users/zsh-completions "${ZSH_CUSTOM:-${ZSH:-${HOME}/.oh-my-zsh}/custom}/plugins/zsh-completions"
-    echo 'fpath+=${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions/src' >>"${HOME}/.zshrc"
+    git clone https://github.com/zsh-users/zsh-completions "${ZSH_CUSTOM:-${ZSH:-$HOME/.oh-my-zsh}/custom}/plugins/zsh-completions"
+    echo 'fpath+=${ZSH_CUSTOM:-${ZSH:-$HOME/.oh-my-zsh}/custom}/plugins/zsh-completions/src' >>"$HOME/.zshrc"
     # zsh-autosuggestion https://github.com/zsh-users/zsh-autosuggestions/blob/master/INSTALL.md
-    git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-${ZSH:-${HOME}/.oh-my-zsh}/custom}/plugins/zsh-autosuggestions"
-    sed -ri 's/^\s*plugins=\((.*)\)/plugins=(\1 zsh-autosuggestions)/g' "${HOME}/.zshrc"
+    git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-${ZSH:-$HOME/.oh-my-zsh}/custom}/plugins/zsh-autosuggestions"
+    sed -ri 's/^\s*plugins=\((.*)\)/plugins=(\1 zsh-autosuggestions)/g' "$HOME/.zshrc"
     # zsh-syntax-highliting https://github.com/zsh-users/zsh-syntax-highlighting/blob/master/INSTALL.md
     git clone 'https://github.com/zsh-users/zsh-syntax-highlighting.git' "${ZSH_CUSTOM:-${ZSH:-$HOME/.oh-my-zsh}/custom}/plugins/zsh-syntax-highlighting"
-    echo '. ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' >>"${HOME}/.zshrc"
+    echo '. ${ZSH_CUSTOM:-${ZSH:-$HOME/.oh-my-zsh}/custom}/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' >>"$HOME/.zshrc"
     # zsh-history-substring-search https://github.com/zsh-users/zsh-history-substring-search
-    git clone https://github.com/zsh-users/zsh-history-substring-search "${ZSH_CUSTOM:-${ZSH:-${HOME}/.oh-my-zsh}/custom}/plugins/zsh-history-substring-search"
-    sed -ri 's/^\s*plugins=\((.*)\)/plugins=(\1 zsh-history-substring-search)/g' "${HOME}/.zshrc"
+    git clone https://github.com/zsh-users/zsh-history-substring-search "${ZSH_CUSTOM:-${ZSH:-$HOME/.oh-my-zsh}/custom}/plugins/zsh-history-substring-search"
+    sed -ri 's/^\s*plugins=\((.*)\)/plugins=(\1 zsh-history-substring-search)/g' "$HOME/.zshrc"
 else
     ### if not exist oh-my-zsh, use zinit # TODO use sheldon
     echo 'y' | bash -c "$(curl -fsSL https://git.io/zinit-install)"
@@ -43,10 +43,10 @@ else
         echo 'zinit light zsh-users/zsh-autosuggestions'
         echo 'zinit light zsh-users/zsh-syntax-highlighting'
         echo 'zinit light zsh-users/zsh-history-substring-search'
-    } >>"${HOME}/.zshrc"
+    } >>"$HOME/.zshrc"
 fi
 
 # purepower and some settings
-curl -fsL 'https://github.com/romkatv/dotfiles-public/raw/master/.purepower' -o "${HOME}/.purepower"
-curl -fsL 'https://github.com/hayas1/dotfiles/raw/master/.devcontainer/resources/.p10k.zsh' -o "${HOME}/.p10k.zsh"
-echo '[ -e ~/.p10k.zsh ] && source ~/.p10k.zsh' >>"${HOME}/.zshrc"
+curl -fsL 'https://github.com/romkatv/dotfiles-public/raw/master/.purepower' -o "$HOME/.purepower"
+curl -fsL 'https://github.com/hayas1/dotfiles/raw/master/.devcontainer/resources/.p10k.zsh' -o "$HOME/.p10k.zsh"
+echo '[ -e $HOME/.p10k.zsh ] && source $HOME/.p10k.zsh' >>"$HOME/.zshrc"
