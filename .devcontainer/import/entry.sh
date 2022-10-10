b8e96f308ccb4e28140e6f12f8c8ac5cd189c5c4
hayas1 <h4ystack@gmail.com>
2022-10-10 00:52:47 +0000

    use docker-from-docker

--- .devcontainer/import/entry.sh
+++ .devcontainer/import/entry.sh
@@ -1,11 +1,16 @@
 #! /bin/bash -e
 
-# for develop with go
+### for develop with go
 if type go >/dev/null 2>&1; then
     go install -v golang.org/x/tools/gopls@latest
 fi
 
-# for develop with python
+### for develop with python
 if [ -e "$WORKSPACE/requirements.txt" ]; then
     pip install -r "$WORKSPACE/requirements.txt"
 fi
+
+### for docker-from-docker
+export DOCKER_BUILDKIT=1
+sudo apt-get update && curl -fsL "https://github.com/microsoft/vscode-dev-containers/raw/main/script-library/docker-debian.sh" | sudo bash
+/usr/local/share/docker-init.sh
