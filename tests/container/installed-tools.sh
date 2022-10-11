02b8f4d2e97cf652f594e4826665867d5443313c
hayas1 <h4ystack@gmail.com>
2022-10-11 13:12:19 +0000

    add docker installed test

--- tests/container/installed-tools.sh
+++ tests/container/installed-tools.sh
@@ -3,4 +3,5 @@ python --version && pip --version &&
     npm --version && # nvm --version  && # nvm is shell function
     go version &&
     cargo --version && rustc --version && rustup --version &&
+    docker --help &&
     echo q | gcloud --help && kubectl --help && helm --help
