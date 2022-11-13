5a8555b0128bc3bbfebea19ad6562a2c50c8c528
hayas1 <h4ystack@gmail.com>
2022-11-13 23:24:28 +0000

    modify public script's path

--- scripts/public/docker/install-gcloud-kubectl-helm.sh
+++ scripts/public/docker/install-gcloud-kubectl-helm.sh
@@ -3,7 +3,7 @@ set -e
 
 apt-get update -y && apt-get install -y gnupg &&
     apt-get clean && rm -rf /var/lib/apt/lists
-curl -fsL 'https://github.com/hayas1/dotfiles/raw/master/.devcontainer/import/build/gcloud-kubectl-helm.sh' | bash -s
+curl -fsL 'https://github.com/hayas1/dotfiles/raw/master/.devcontainer/build/gcloud-kubectl-helm.sh' | bash -s
 
 {
     echo '### gcloud kubectl helm completion setting'
