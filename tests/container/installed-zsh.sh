cac02e5fed9c5619ed0d9c06229463b9df060fb6
hayas1 <h4ystack@gmail.com>
2023-01-17 14:12:00 +0000

    path

--- tests/container/installed-zsh.sh
+++ tests/container/installed-zsh.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 
 zsh -c exit && test -e "${HOME}/.oh-my-zsh/custom/themes/powerlevel10k/internal/p10k.zsh" &&
-    diff "${WORKSPACE}/.devcontainer/resources/.p10k.zsh" "${HOME}/.p10k.zsh" 1>&2 &&
-    diff "${WORKSPACE}/.devcontainer/resources/.bash_aliases" "${HOME}/.bash_aliases" 1>&2
+    # diff "${WORKSPACE}/public/docker/resources/.zshrc" "${HOME}/.zshrc" 1>&2 &&
+    diff "${WORKSPACE}/public/docker/resources/.p10k.zsh" "${HOME}/.p10k.zsh" 1>&2 &&
+    diff "${WORKSPACE}/public/docker/resources/.bash_aliases" "${HOME}/.bash_aliases" 1>&2
