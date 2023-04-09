9712989817e8012400913055cd7b9777a4af005e
hayas1 <40286751+hayas1@users.noreply.github.com>
2023-04-09 22:31:48 +0900

    Revert "Refactor/devcontainer for dotfiles develop"

--- tests/container/installed.sh
+++ tests/container/installed.sh
@@ -1,8 +1,4 @@
 #! /bin/sh
 
 DIR=$(dirname "$(realpath "$0")")
-TESTS=$(dirname "$DIR")
-REPOSITORY=$(dirname "$TESTS")
-
-python "$TESTS/build/postbuild.py" test-installed-extensions "$REPOSITORY/.devcontainer/devcontainer.json" &&
-    "$DIR"/installed-zsh.sh && "$DIR"/installed-tools.sh && "$DIR"/installed-hello.sh
+"$DIR"/installed-zsh.sh && "$DIR"/installed-tools.sh && "$DIR"/installed-hello.sh
