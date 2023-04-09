c53b0f61bb3a6dc60c02767215e3cc127d9123c0
hayas1 <h4ystack@gmail.com>
2023-04-10 01:47:44 +0900

    test

--- tests/container/installed.sh
+++ tests/container/installed.sh
@@ -4,6 +4,7 @@ DIR=$(dirname "$(realpath "$0")")
 TESTS=$(dirname "$DIR")
 REPOSITORY=$(dirname "$TESTS")
 
-pip install -r "$TESTS/build/requirements.txt"
-python "$TESTS/build/postbuild.py" test-installed-extensions "$REPOSITORY/.devcontainer/devcontainer.json" &&
-    "$DIR"/installed-zsh.sh && "$DIR"/installed-tools.sh && "$DIR"/installed-hello.sh
+# devcontainer-cli may not install vscode-server
+# pip install -r "$TESTS/build/requirements.txt"
+# python "$TESTS/build/postbuild.py" test-installed-extensions "$REPOSITORY/.devcontainer/devcontainer.json"
+"$DIR"/installed-zsh.sh && "$DIR"/installed-tools.sh && "$DIR"/installed-hello.sh
