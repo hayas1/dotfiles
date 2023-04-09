35ae93543f3bcee3f7bb61127a25552438178502
hayas1 <h4ystack@gmail.com>
2023-04-09 13:07:19 +0000

    refactor test

--- tests/container/installed-hello.sh
+++ tests/container/installed-hello.sh
@@ -3,7 +3,7 @@
 DIR=$(dirname "$(realpath "$0")")
 SAMPLE=$(dirname "$DIR")/sample/
 TMP=$(mktemp -dt "dotfiles-test-hello-$(date +%Y%m%d%H%M%S)-XXXXXX")
-[ "$(python "$SAMPLE"/hello.py)" = "Hello World" ]
-[ "$(node "$SAMPLE"/hello.js)" = "Hello World" ]
-[ "$(go run "$SAMPLE"/hello.go)" = "Hello World" ]
-[ "$(rustc -o "$TMP"/hello-rs "$SAMPLE"/hello.rs && "$TMP"/hello-rs)" = "Hello World" ]
+[ "$(python "$SAMPLE"/hello.py)" = "Hello World" ] &&
+    [ "$(node "$SAMPLE"/hello.js)" = "Hello World" ] &&
+    [ "$(go run "$SAMPLE"/hello.go)" = "Hello World" ] &&
+    [ "$(rustc -o "$TMP"/hello-rs "$SAMPLE"/hello.rs && "$TMP"/hello-rs)" = "Hello World" ]
