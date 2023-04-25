6a3f75103a125f35951f59176658b67ee5b02449
hayas1 <h4ystack@gmail.com>
2023-04-25 14:05:42 +0000

    merge

--- public/build/install-starship-sheldon.sh
+++ public/build/install-starship-sheldon.sh
@@ -1,5 +1,8 @@
 #! /bin/bash -e
 
+# https://starship.rs/#quick-install
+curl -fsL https://starship.rs/install.sh | sh -s -- -y
+
 # https://sheldon.cli.rs/Installation.html#pre-built-binaries
 curl --proto '=https' -fLsS https://rossmacarthur.github.io/install/crate.sh |
     bash -s -- --repo rossmacarthur/sheldon --to /usr/local/bin
@@ -7,5 +10,7 @@ curl --proto '=https' -fLsS https://rossmacarthur.github.io/install/crate.sh |
 cat <<'EOF' >>"${HOME}/.zshrc"
 ### load sheldon
 eval "$(sheldon source)"
+### load starship
+eval "$(starship init zsh)"
 
 EOF
