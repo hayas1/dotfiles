ead4e1848e3c55b3113d2d7f156a330b8675185e
hayas1 <h4ystack@gmail.com>
2023-04-25 15:39:28 +0000

    multi byte char

--- public/build/install-starship-sheldon.sh
+++ public/build/install-starship-sheldon.sh
@@ -7,6 +7,10 @@ curl -fsL https://starship.rs/install.sh | sh -s -- -y
 curl --proto '=https' -fLsS https://rossmacarthur.github.io/install/crate.sh |
     bash -s -- --repo rossmacarthur/sheldon --to /usr/local/bin
 
+# multi bytes
+apt-get install -y locales
+localedef -f UTF-8 -i en_US en_US.UTF-8
+
 cat <<'EOF' >>"${HOME}/.zshrc"
 ### load sheldon
 eval "$(sheldon source)"
