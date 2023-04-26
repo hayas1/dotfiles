0b697de93773c257edd3d4bd6e92bf21ac74a791
hayas1 <h4ystack@gmail.com>
2023-04-26 11:38:30 +0000

    comment

--- public/build/install-pyenv.sh
+++ public/build/install-pyenv.sh
@@ -5,6 +5,7 @@ git clone https://github.com/pyenv/pyenv.git "${HOME}/.pyenv"
 pushd "${HOME}/.pyenv" && src/configure && make -C src && popd
 
 {
+    echo '### pyenv settings'
     echo 'export PYENV_ROOT="$HOME/.pyenv"'
     echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"'
     echo 'eval "$(pyenv init -)"'
